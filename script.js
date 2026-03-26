65ff32da630e283328c0f8e2655697513e1429af
Krish Ghori <krishghori90@gmail.com>
2026-03-26 16:02:41 +0530

    Add variable declarations and type checking

---
+++ script.js
@@ -0,0 +1,12 @@
+// const example
+const R = "krish";   // value cannot be changed
+console.log(R);
+
+// let example
+let age = "krish";   // value can be changed
+age = 18;
+console.log(age);
+
+// type checking
+console.log(typeof(age));
+console.log(typeof(R));
