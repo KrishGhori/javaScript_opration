ac3cf1e5105947890e6977f8e4c3b01180578539
Krish Ghori <krishghori90@gmail.com>
2026-03-26 16:05:09 +0530

    Add arithmetic operators examples

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ console.log(age);
 // type checking
 console.log(typeof(age));
 console.log(typeof(R));
+
+let a = 10;
+let b = 5;
+
+console.log(a + b);  // addition
+console.log(a - b);  // subtraction
+console.log(a * b);  // multiplication
+console.log(a / b);  // division
+console.log(a % b);  // remainder
+console.log(a ** b); // power
